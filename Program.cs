ba5503ec9f1044a9d195e6cef106525dbbf7176a
Shinji Shioda <79740804+ShinjiShioda@users.noreply.github.com>
2023-07-19 15:46:32 +0900

    add Help

--- Program.cs
+++ Program.cs
@@ -12,21 +12,26 @@ public static unsafe class Program {
 
     public static int Main (string[] argv) {
         if (argv.Length < 2) {
-            Console.WriteLine ("Error:No/Few Arg!");
-            Console.WriteLine ("Usage:ExecML CX_HEX_Value DX_HEX_Value ML_HEX ...");
+            if ((argv.Length == 1) && (argv[0] == "-h" || argv[0] == "-?" | argv[0] == "-help" | argv[0] == "--help")) {
+                Console.WriteLine ("HELP");
+            } else {
+                Console.WriteLine ("Error:Few Arg!");
+            }
+            Console.WriteLine ("Usage:ExecML ECX_HEX_Value EDX_HEX_Value ML_HEX ...");
             Console.WriteLine ("Ex:ExecML 80000000 10 0f bc c1 0f bd da 88 dc c3");
             Environment.Exit (1);
         }
         List<byte> bin = new List<byte> ();
         byte[] asm;
-        uint CX = Convert.ToUInt32 (argv[0], 16);
-        uint DX = Convert.ToUInt32 (argv[1], 16);
+        uint ECX = Convert.ToUInt32 (argv[0], 16);
+        uint EDX = Convert.ToUInt32 (argv[1], 16);
         try {
             foreach (string x in argv.Skip (2)) { bin.Add (Convert.ToByte (x, 16)); }
         } catch {
+            Console.WriteLine ("Error:Wrong Arg!");
             Environment.Exit (1);
         }
-        bin.Add (0xC3); // C3=Ret. for safty. For to Able to run argv only CX,DX
+        bin.Add (0xC3); // C3=Ret. for safty. For to Able to run argv only ECX,EDX
         asm = bin.ToArray ();
         void * buffer = VirtualAlloc (null, asm.Length, 0x1000, 4);
         var func = (delegate * < uint, uint, uint >) buffer;
@@ -35,7 +40,7 @@ public static unsafe class Program {
         VirtualProtect (buffer, asm.Length, 0x20, & dummy);
         try {
             unchecked {
-                uint r = func (CX, DX);
+                uint r = func (ECX, EDX);
                 Console.WriteLine ($"EAX={r.ToString("X8")}");
             }
             VirtualFree (buffer, 0, 0x8000);
